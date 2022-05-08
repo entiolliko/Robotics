14eb9dc355bf1731a3ce203be1a2905729e8727b
entiolliko <entiol.liko@mail.polimi.it>
2022-05-08 09:33:08 +0200

    Paramters alibration

--- src/data.h
+++ src/data.h
@@ -1,5 +1,5 @@
-#define R 0.0775
-#define W 0.175
-#define L 0.200
-#define GEAR_RATIO 5.0	
-#define CPR 42.0
+#define R 0.076 //0.7
+#define W 0.200 //0.2
+#define L 0.2 //0.169
+#define GEAR_RATIO 5.0	//5
+#define CPR 43.0 //42
